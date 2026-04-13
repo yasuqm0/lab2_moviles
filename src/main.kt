b80a56e5bd6db6dc7774c2e3166446f6d895b3ca
yasu <yasuhigkx@gmail.com>
2026-04-12 23:44:29 -0500

    ola este es el primer commit

---
+++ src/main.kt
@@ -0,0 +1,41 @@
+import acuario.*
+
+fun construirAcuario() {
+
+    // 8
+    val acuario8 = Acuario(largo = 25, ancho = 25, alto = 40)
+    acuario8.imprimirTamano()
+    val miTorre = TanqueTorre(diametro = 25, alto = 40)
+    miTorre.imprimirTamano()
+
+}
+
+fun main() {
+    construirAcuario()
+}
+
+/*
+    miAcuario.alto = 60
+    miAcuario.imprimirTamano()
+
+    val acuario1 = Acuario()
+    acuario1.imprimirTamano()
+    // altura y largo por defecto
+    val acuario2 = Acuario(ancho = 25)
+    acuario2.imprimirTamano()
+    // ancho por defecto
+    val acuario3 = Acuario(alto = 35, largo = 110)
+    acuario3.imprimirTamano()
+    // todo personalizado
+    val acuario4 = Acuario(ancho = 25, alto = 35, largo = 110)
+    acuario4.imprimirTamano()
+    // nuevo constructor
+    val acuario6 = Acuario(numeroDePeces = 29)
+    acuario6.imprimirTamano()
+    println("Volumen: ${acuario6.ancho * acuario6.largo * acuario6.alto / 1000} l")
+    // 7
+    val acuario7 = Acuario(numeroDePeces = 29)
+    acuario7.imprimirTamano()
+    acuario7.volumen = 70
+    acuario7.imprimirTamano()
+*/
